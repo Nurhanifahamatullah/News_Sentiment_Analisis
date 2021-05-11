4c46d677b665cb69963c3fb60baa8ea62e34703c
Nurhanifahamatullah <nurhanifahamatullah@gmail.com>
2021-05-11 10:31:23 +0700

    nurhanifah_1184086

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# News_Sentiment_Analisis
+# News Sentimen Analisis
+# (Nur Hanifah Amatullah_1184086)
+## Aplikasi digunakan untuk menganalisis text dari sebuah url berita yang nantinya akan menghasilkan nilai polaritas dan nilai subjektivitas dari berita tersebut.
+## Cara penggunaan aplikasi ini yaitu :
+## 1. Kita disuruh memasukkan link url berita yang ingin kita ketahui analisis sentimentnya
+## 2. Lalu klik submit
+## 3. Setelah itu akan keluar nilai polarity, categori sentiment polaritynya, categori sentiment subjectivity dan nilai subjectivitynya.
---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request
+from textblob import TextBlob
+import re
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+
+def index():
+    if request.method == 'POST':
+        url="https://economictimes.indiatimes.com/news/india/high-court-come-down-on-publicity-interest-litigations-imposes-cost"
+        remove = url.replace(('-'),' ')
+        change = r"https?://(www\.)?"
+        text = re.sub(change, ' ', remove)
+        sentiment = TextBlob(text).sentiment
+        polarty = sentiment.polarity
+        subjct = sentiment.subjectivity
+        
+#polarty   
+        if polarty > 0.75:
+            sentiment_polarty = "Extremely positive."
+        elif polarty > 0.5:
+            sentiment_polarty = "Significantly positive."
+        elif polarty > 0.3:
+            sentiment_polarty = "Fairly positive."
+        elif polarty > 0.1:
+            sentiment_polarty = "Slightly positive."
+        elif polarty == 0:
+            sentiment_polarty = "Netral."
+        elif polarty < -0.1:
+            sentiment_polarty = "Slightly negative."
+        elif polarty < -0.3:
+            sentiment_polarty = "Fairly negative."
+        elif polarty < -0.5:
+            sentiment_polarty = "Significantly negative."
+        elif polarty < -0.75:
+            sentiment_polarty = "Extremely negative."
+        else:
+            sentiment_polarty = "no sentiment"
+            
+#subjct
+        if subjct > 0.75:
+            sentiment_subjct = "Extremely subjective."
+        elif subjct > 0.5:
+            sentiment_subjct = "Fairly subjective."
+        elif subjct > 0.3:
+            sentiment_subjct = "Fairly objective."
+        elif subjct > 0.1:
+            sentiment_subjct = "Extremely objective."
+        elif subjct == 0:
+            sentiment_subjct = "Netral."
+        else:
+            sentiment_subjct = 'no sentiment.' 
+        
+        return render_template('index.html', url=url, polarty=polarty, sentiment_polarty=sentiment_polarty, sentiment_subjct=sentiment_subjct, subjct=subjct)
+    return render_template('layout.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
